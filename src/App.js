68afa90165ce243cfd7b748fbe15fa29bdd7f98a
LobnaDRISSI <lobnadrissi2020@gmail.com>
2022-02-15 12:00:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import './index.css';
+import BackgroundImagePage from './BackgroundImagePage';
+
+import Forms from './Forms.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
+
+function App() {
+  return (
+    <div>
+
+
+<Forms />
+
+    </div>
+  );
+}
+
+export default App;
